a69094e35697f5604f7ed2d43282afcc59d494a8
jojo <elokajosephine10@gmail.com>
2026-05-01 10:33:16 -0700

    update app

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 const AWS = require("aws-sdk");
 const http = require("http");
 
-const sqs = new AWS.SQS({ region: process.env.AWS_REGION });
+const sqs = new AWS.SQS({
+  region: process.env.AWS_REGION,
+});
 
 const QUEUE_URL = process.env.QUEUE_URL;
 
 const server = http.createServer(async (req, res) => {
-  const params = {
-    QueueUrl: QUEUE_URL,
-    MessageBody: "Job from web 🚀",
-  };
+  if (req.url === "/") {
+    try {
+      const params = {
+        QueueUrl: QUEUE_URL,
+        MessageBody: "Job from web 🚀",
+      };
 
-  await sqs.sendMessage(params).promise();
+      await sqs.sendMessage(params).promise();
 
-  res.end("Job sent 🚀");
+      console.log("Message sent to SQS");
+
+      res.end("Job sent 🚀");
+    } catch (err) {
+      console.error("SQS error:", err);
+      res.statusCode = 500;
+      res.end("Error sending job");
+    }
+  } else {
+    res.end("OK");
+  }
 });
 
 server.listen(3000, () => {
-  console.log("Server running");
+  console.log("Server running on port 3000");
+  console.log("QUEUE_URL:", QUEUE_URL);
 });
